0a025d583c88e1a1a7360e45efadf89f6086dd04
Paul Oke <Poke@sbsc.com>
2023-10-07 11:47:03 +0100

    Added Azure Cosmos Db

---
+++ CapitalPlacementTask.API/ServiceExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace CapitalPlacementTask.API
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCosmosDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add CosmosClient and configure logging
+            services.AddSingleton((provider) =>
+            {
+                var endpointUri = configuration["CosmosDbSettings:EndpointUri"];
+                var primaryKey = configuration["CosmosDbSettings:PrimaryKey"];
+                var databaseName = configuration["CosmosDbSettings:DatabaseName"];
+
+                var cosmosClientOptions = new CosmosClientOptions
+                {
+                    ApplicationName = databaseName,
+                    ConnectionMode = ConnectionMode.Gateway,
+                };
+
+                var loggerFactory = LoggerFactory.Create(builder =>
+                {
+                    builder.AddConsole();
+                });
+
+                var cosmosClient = new CosmosClient(endpointUri, primaryKey, cosmosClientOptions);
+
+
+                return cosmosClient;
+            });
+        }
+
+        public static void AddServices(this IServiceCollection services, IWebHostEnvironment hostingEnvironment, IConfiguration configuration)
+        {
+            //services.AddScoped<IProgramDetailService, ProgramDetailService>();  
+        }
+
+
+    }
+}
